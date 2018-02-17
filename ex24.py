e466aea382ba478b63233bae98f968596d27e045
Jeong YunWon <jeong@youknowone.org>
2018-02-17 18:21:53 +0900

    ex24

--- ex24.py
+++ ex24.py
@@ -1,7 +1,7 @@
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
+print("모든 것을 연습해 봅시다.")
+print('\\를 이용해 \n 새줄이나 \t 탭을 하는 탈출순서열에 대해 \'알아야만\' 합니다.')
 
-poem = """
+시 = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
@@ -11,32 +11,31 @@ and requires an explanation
 """
 
 print("--------------")
-print(poem)
+print(시)
 print("--------------")
 
 
-five = 10 - 2 + 3 - 6
-print(f"This should be five: {five}")
+다섯 = 10 - 2 + 3 - 6
+print("이 값은 다섯입니다: {다섯}")
 
-def secret_formula(started):
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
-    crates = jars / 100
-    return jelly_beans, jars, crates
+def 비밀_공식(시작):
+    젤리 = 시작 * 500
+    그릇 = 젤리 / 1000
+    상자 = 그릇 / 100
+    return 젤리, 그릇, 상자
 
 
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+시작점 = 10000
+젤리, 그릇, 상자 = 비밀_공식(시작점)
 
-# remember that this is another way to format a string
-print("With a starting point of: {}".format(start_point))
-# it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+# 문자열을 포맷하는 다른 방법이었습니다
+print("시작점: {}".format(시작점))
+# f"" 문자열을 쓰는것과 같지요
+print(f"젤리 {젤리}알, {그릇}그릇, {상자}상자가 있습니다.")
 
-start_point = start_point / 10
+시작점 = 시작점 / 10
 
-print("We can also do that this way:")
-formula = secret_formula(start_point)
-# this is an easy way to apply a list to a format string
-print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
-
+print("이렇게 할 수도 있습니다.")
+공식 = 비밀_공식(시작점)
+# 리스트를 포맷 문자열에 적용하는 쉬운 방법이지요
+print("젤리 {}알, {}그릇, {}상자가 있습니다.".format(*공식))
