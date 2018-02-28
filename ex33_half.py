62bc6c926f6551cd5a9816e9da467d477a1b780a
Jeong YunWon <jeong@youknowone.org>
2018-02-28 23:06:59 +0900

    ex33

--- ex33_half.py
+++ ex33_half.py
@@ -1,17 +1,17 @@
 i = 0
-숫자들 = []
+numbers = []
 
 while i < 6:
     print(f"꼭대기에서 i는 {i}")
-    숫자들.append(i)
+    numbers.append(i)
 
     i = i + 1
-    print("숫자는 이제: ", 숫자들)
+    print("숫자는 이제: ", numbers)
     print(f"바닥에서 i는 {i}")
 
 
 print("숫자: ")
 
-for 숫자 in 숫자들:
-    print(숫자)
+for num in numbers:
+    print(num)
 
