3e684d6b1010ff9937278c18c74b748512426134
Jeong YunWon <jeong@youknowone.org>
2018-02-28 22:31:26 +0900

    ex29

--- ex29_half.py
+++ ex29_half.py
@@ -1,30 +1,30 @@
 
-사람 = 20
-고양이 = 30
-개 = 15
+people = 20
+cats = 30
+dogs = 15
 
 
-if 사람 < 고양이:
+if people < cats:
     print("고양이가 너무 많아요! 세상은 멸망합니다!")
 
-if 사람 > 고양이:
+if people > cats:
     print("고양이가 많지 않아요! 세상은 지속됩니다!")
 
-if 사람 < 개:
+if people < dogs:
     print("세상은 침에 젖습니다!")
 
-if 사람 > 개:
+if people > dogs:
     print("세상은 말랐습니다!")
 
 
-개 += 5
+dogs += 5
 
-if 사람 >= 개:
+if people >= dogs:
     print("사람은 개보다 많거나 같습니다")
 
-if 사람 <= 개:
+if people <= dogs:
     print("사람은 개보다 적거나 같습니다.")
 
 
-if 사람 == 개:
+if people == dogs:
     print("사람은 개입니다.")
